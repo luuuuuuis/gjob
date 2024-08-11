61be3e6d4f6ae8c9a4e45d234847d8da8d3b11f8
Luis <luis9527@163.com>
2024-08-11 22:42:03 +0800

    初始化

--- admin/web/src/views/home/dashboard.vue
+++ admin/web/src/views/home/dashboard.vue
@@ -9,16 +9,18 @@
           <lay-input size="xs" allow-clear=true prefix-icon="layui-icon-search" placeholder="任务名/用户/系统"></lay-input>
           <lay-button id="searchButton" type="primary"><lay-icon type="layui-icon-search"></lay-icon></lay-button>
         </div>
+        
       </lay-header>
+      <lay-page-header class="goback" content="详情页面" @back="handleBack"></lay-page-header>
     </div>
     
     <div class="main-container">
       <lay-side>
-        <lay-menu :selected-key="selectedKey" @change-selected-Key="changeSelectedKey" @change-open-keys="changeOpenKeys" v-model:openKeys="openKeys2" :tree="true">
-          <lay-menu-item id="1"><router-link to="/nodelist">工作节点</router-link></lay-menu-item>
-          <lay-menu-item id="2"><router-link to="/sqllist">SQL监控</router-link></lay-menu-item>
-          <lay-menu-item id="3"><router-link to="/scriptlist">脚本监控</router-link></lay-menu-item> 
-          <lay-menu-item id="3"><router-link to="/grouplist">告警通知组</router-link></lay-menu-item> 
+        <lay-menu class="sidemenu" :tree="true">
+          <lay-menu-item class="sidemenudetail" id="1"><router-link to="/nodelist">工作节点</router-link></lay-menu-item>
+          <lay-menu-item class="sidemenudetail" id="2"><router-link to="/sqllist">SQL监控</router-link></lay-menu-item>
+          <lay-menu-item class="sidemenudetail" id="3"><router-link to="/scriptlist">脚本监控</router-link></lay-menu-item> 
+          <lay-menu-item class="sidemenudetail" id="4"><router-link to="/grouplist">告警通知组</router-link></lay-menu-item> 
         </lay-menu>
       </lay-side>
 
@@ -30,13 +32,19 @@
 </template>
 
 <script>
-// 组件逻辑
-export default {
-  // ...
-}
+  const handleBack=()=>{
+    alert('点击返回回调')
+  }
 </script>
 
 <style>
+  .goback {
+    /* float: left; */
+    /* 离左边60 */
+    margin-left: 133px;
+    /* 靠下 */
+    margin-top: 20px;
+  }
   .title {
     float: left;
     font-size: 24px;
@@ -50,10 +58,14 @@ export default {
     height: 100vh;
   }
 
+  .sidemenudetail {
+    border-bottom: 2px solid #02205b57; /* 1px宽度的实线黑色边框 */
+  }
+
   .layout-container .header-container {
     line-height: 60px;
     text-align: center;
-    background: #87ca9a;
+    background: #16baaa;
     color: white;
   }
 
@@ -65,16 +77,20 @@ export default {
 
   .main-container .layui-side {
     /* 侧边栏样式 */
-    background: #77c38c;
+    background: #16b777;
     color: white;
     text-align: center;
+  }
+  /* 导航栏样式 */
+  .sidemenu {
+    background: #16b777;
   }
 
   .main-container .layui-body {
     /* body内容区域样式 */
     flex: 1; /* 占据剩余空间 */
-    background: #5FB878;
-    color: white;
+    background: #f8f8f8;
+    color: rgb(7, 7, 7);
   }
 
   .layui-input-inline {
--- admin/web/src/views/home/nodelist.vue
+++ admin/web/src/views/home/nodelist.vue
@@ -1,45 +1,57 @@
 <template>
-    <div class="alert-container">
-      <h2>工作节点</h2>
-      <ul class="alert-list">
-        <li v-for="alert in alerts" :key="alert.id">
-          {{ alert.name }}
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
+  <div class="alert-container">
+    <lay-table :columns="columns2" :data-source="dataSource2" v-model:expandKeys="expandKeys2">
+    </lay-table>
+  </div>
+</template>
+
+<script>
   export default {
-    name: 'Alert',
-    data() {
+    setup() {
+
+      const columns2 = [
+        { title:"编号", width:"80px", key:"id", fixed: "left", sort: "desc"},
+        { title:"名称", width:"120px", key:"name" },
+        { title:"IP", width:"80px", key:"ip"},
+        { title:"状态", width:"80px", key:"status"},
+      ]
+
+      const dataSource2 = [
+        {id:"1", name:"门户", ip:"192.168.1.3", status: true},
+        {id:"2", name:"大数据", ip:"192.168.1.4", status: false},
+      ]
+
       return {
-        alerts: [
-          { id: 1, name: '工作节点1' },
-          { id: 2, name: '工作节点2' },
-          // 更多告警组...
-        ]
-      };
+        columns2,
+        dataSource2,
+      }
     }
-  };
-  </script>
+  }
+</script>
   
-  <style scoped>
+<style scoped>
   .alert-container {
     padding: 20px;
   }
-  
+
   .alert-list {
     list-style: none;
     padding: 0;
   }
-  
+
   .alert-list li {
     padding: 8px;
     border-bottom: 1px solid #eee;
   }
-  
+
   .alert-list li:last-child {
     border-bottom: none;
   }
-  </style>
+  .expand-content {
+    width: 100%;
+    padding: 0px 20px 20px 0px;
+    .layui-progress {
+      margin-top: 24px;
+    }
+  }
+</style>
